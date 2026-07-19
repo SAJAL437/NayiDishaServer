704b59ce363d1491f4bb70bcb19541182ede7c99
sajal tiwari <sajaltiwari437@gmail.com>
2026-07-19 21:37:52 +0530

    update

--- src/main/java/com/impact/entity/ReportIssue.java
+++ src/main/java/com/impact/entity/ReportIssue.java
@@ -22,7 +22,7 @@ public class ReportIssue {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String name;  
     private String email;
     private String phoneNumber;
     private String title;
